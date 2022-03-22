d25f6a7da9e30df7ef866c49232fbd0aada722f0
PatriciaDivi <patriciavianad@gmail.com>
2022-03-22 17:20:31 -0300

    1 Requisito |  Teste o componente <App.js /> | :rocket:

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../services/renderWithRouter';
+import App from '../App';
+
+describe('Teste se o topo da aplicação contém um conjunto fixo de links de navegação',
+
+  () => {
+    test('O primeiro link deve possuir o texto HOME', () => {
+      renderWithRouter(<App />);
+
+      const linkHome = screen.getByRole('link', { name: /Home/i }); // RTH
+      expect(linkHome).toBeDefined();
+    });
+
+    test('O primeiro link deve possuir o texto ABOUT', () => {
+      renderWithRouter(<App />);
+
+      const linkAbout = screen.getByRole('link', { name: /About/i });
+      expect(linkAbout).toBeDefined();
+    });
+
+    test('O primeiro link deve possuir o texto FAVORITE POKÉMONS', () => {
+      renderWithRouter(<App />);
+
+      const linkAFavoritePokemons = screen.getByRole('link',
+        { name: /Favorite Pokémons/i });
+      expect(linkAFavoritePokemons).toBeDefined();
+    });
+  });
