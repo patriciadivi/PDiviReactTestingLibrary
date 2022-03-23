f7d8a28fcccb4f6fd163b7d64d06375376f3069f
PatriciaDivi <patriciavianad@gmail.com>
2022-03-22 23:44:05 -0300

    4 Requisito | Teste o componente <NotFound.js /> | :rocket: | concluido

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './Helpers/renderWithRouter';
+import App from '../App';
+
+describe('Testando a Página NOT FOUND', () => {
+  test('Teste se página contém um heading h2 com o texto Page requested not found 😭',
+    () => {
+      const { history } = renderWithRouter(<App />);
+      history.push('/notfound');
+      const headingNotFound = screen
+        .getByRole('heading', { name: /Page requested not found/i, level: 2 });
+      expect(headingNotFound).toBeDefined();
+    });
+
+  test('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/notfound');
+
+    const imgCompGif = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const verImgGif = screen
+      .getByRole('img',
+        {
+          name: /Pikachu crying because the page requested was not found/i,
+        });
+
+    expect(verImgGif.src).toBe(imgCompGif);
+  });
+});
