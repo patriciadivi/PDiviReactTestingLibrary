3940a821b49e84b3b39eb17c15f3d5d7e073f152
PatriciaDivi <patriciavianad@gmail.com>
2022-03-22 22:45:45 -0300

    3 Requisto | Teste o componente <FavoritePokemons.js /> | :rocket: | Concluido

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from './Helpers/renderWithRouter';
+import App from '../App';
+
+describe('', () => {
+  test('Teste se é exibido na tela a mensagem No favorite pokemon found',
+    () => {
+      const { history } = renderWithRouter(<App />);
+      history.push('/favorites');
+      const NotFavorite = screen
+        .getByText(/No favorite pokemon found/i);
+      expect(NotFavorite).toBeDefined();
+    });
+});
